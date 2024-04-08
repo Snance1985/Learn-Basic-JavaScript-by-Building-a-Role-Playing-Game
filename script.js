d8461ef47e7df25abb12224d35d7339ece21f04c
Sean <131737726+Snance1985@users.noreply.github.com>
2024-04-08 07:36:40 -0700

    assigned variable inventory to string value of stick

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ Variables can be assigned a value.
 When you do this while you declare it, this is called initialization.
 When a variable name has multiple words, the convention in JavaScript is to use what's called camelCase.
 The first word is lowercase, and the first letter of every following word is uppercase.
+The variables you have assigned have all had values that are numbers.
+JavaScript has multiple different data types.
+The next one you will use is the string. Strings are used to store things like words or text.
+Strings are surrounded with double quotes, single quotes, or backticks.
 */
 let xp = 0;
 let health = 100;
 let gold = 50;
 let currentWeapon = 0;
 let fighting;
-let inventory;
+let inventory = "stick";
 let monsterHealth;

d1ea2f9847b1ddcecc82467c4ecacdd8a2822f6d
Sean <131737726+Snance1985@users.noreply.github.com>
2024-04-07 19:36:43 -0700

    initialized xp variable with 0

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-//In JavaScript, a variable is used to hold a value.
-//To use a variable, you must first declare it.
-let xp;
+/*
+In JavaScript, a variable is used to hold a value.
+To use a variable, you must first declare it.
+Variables can be assigned a value.
+When you do this while you declare it, this is called initialization.
+*/
+let xp = 0;

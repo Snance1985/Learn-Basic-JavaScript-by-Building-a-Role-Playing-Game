0444d4ae9e962171e9156ca065450c946142a1b5
Sean <131737726+Snance1985@users.noreply.github.com>
2024-04-18 19:46:23 -0700

    initialized button1 onclick with function goStore

--- script.js
+++ script.js
@@ -60,3 +60,18 @@ function goCave() {
 function fightDragon() {
     console.log("Fighting dragon.");
 }
+/*
+button1 represents your first button element.
+These elements have a special property called onclick, which you can use to determine what happens when someone clicks that button.
+
+You can access properties in JavaScript a couple of different ways.
+The first is with dot notation.
+Here is an example of using dot notation to set the onclick property of a button to a function reference.
+
+button.onclick = myFunction;
+
+In this example, button is the button element, and myFunction is a reference to a function.
+When the button is clicked, myFunction will be called.
+*/
+// initialize buttons
+button1.onclick = goStore;

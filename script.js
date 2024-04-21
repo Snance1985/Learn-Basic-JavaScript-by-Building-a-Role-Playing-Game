4d0942fbb3cbf8072801d4c6a62927f72daec438
Sean <131737726+Snance1985@users.noreply.github.com>
2024-04-20 19:31:45 -0700

    added innerText and onclick to goTown function

--- script.js
+++ script.js
@@ -62,6 +62,15 @@ info.innerText = "Hello World";
 
 The following example would change the text of the p element from Demo content to Hello World.
 */
+function goTown() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store.";
+  }
 function goStore() {
     button1.onclick = buyHealth;
     button2.onclick = buyWeapon;
@@ -81,9 +90,6 @@ function buyHealth(){
 
 }
 function buyWeapon(){
-
-}
-function goTown(){
 
 }
 /*
